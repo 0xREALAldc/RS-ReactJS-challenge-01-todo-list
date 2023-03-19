0e449eeb6d13196098ec40909f05f0404ce0c050
0xREALaldc <andredonat@hotmail.com>
2023-03-19 10:41:01 -0300

    Add: finished styling the empty list do display, created the styles for the list to display with items. Started to create the functionality for adding a new task.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
-import { PlusCircle, ClipboardText } from '@phosphor-icons/react'
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-import styles from './App.module.css'
 
+import { PlusCircle, 
+  ClipboardText, 
+  Trash,
+  Circle,
+  CheckCircle } from '@phosphor-icons/react'
+  
 import rocketLogo from './assets/rocket.svg'
-
+  
+import styles from './App.module.css'
 import './global.css'
 
+interface Task {
+  id: string;
+  isFinished: boolean;
+  content: string;
+}
+
+const tasks1: Task[] = [
+  {
+    id: 'adadasd1',
+    isFinished: false,
+    content: 'Get the groceries at the supermarket'
+  },
+  {
+    id: 'adadasd2',
+    isFinished: false,
+    content: 'Get a present for my girlfriend'
+  },
+  {
+    id: 'adadasd3',
+    isFinished: true,
+    content: 'Study for 3 hours today'
+  },
+  {
+    id: 'adadasd4',
+    isFinished: false,
+    content: 'Meditate 15 minutes'
+  },
+];
+
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTaskText, setNewTaskText] = useState('')
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+
+    const newTask: Task = {
+      id: 'asdhasu6',
+      content: newTaskText,
+      isFinished: false
+    }
+
+    setTasks([...tasks, newTask])
+    setNewTaskText('')
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value)
+  }
+
   return (
     <div>
       <header className={styles.header}>
@@ -17,28 +72,53 @@ export function App() {
         </div>
       </header>
 
-      <form className={styles.taskForm}>
-        <input type="text" placeholder="Add a new task"/>
+      <form className={styles.taskForm} onSubmit={handleCreateNewTask}>
+        <input 
+          type="text" 
+          placeholder="Add a new task"
+          value={newTaskText}
+          onChange={handleNewTaskChange}
+        />
       
         <button type="submit">
           Criar
-          <PlusCircle/>
+          <PlusCircle size={16}/>
         </button>
       </form>
 
       <main className={styles.taskList}>
         <div className={styles.taskListInfo}>
-          <p>Tasks created <span>0</span></p>
+          <p>Tasks created <span>{tasks.length}</span></p>
           <p>Finished <span>0</span></p>
         </div>
 
-        <div className={styles.taskListItems}>
-          <ClipboardText size={56}/>
-          <div>
-            <strong>You still don't have any tasks</strong>
-            <p>Create a new task and organize your daily life</p>
+        { tasks.length === 0 
+          ?
+          <div className={`${styles.taskListItems} ${styles.taskListItemsEmpty} `}>
+            <ClipboardText size={56}/>
+            <div>
+              <strong>You still don't have any tasks</strong>
+              <p>Create a new task and organize your daily life</p>
+            </div>
           </div>
-        </div>
+          :
+          <div className={styles.taskListItems}>
+            { tasks.map(task => {
+              return (
+                <div className={styles.task} key={task.id}>
+                  <button>
+                    {task.isFinished ? <CheckCircle size={24} /> : <Circle size={24} /> }
+                  </button>
+                  <p>{task.content}</p>
+                  <button>
+                    <Trash size={24}/>
+                  </button>
+                </div>
+              )
+            })}
+          </div>
+        }
+        
       </main>
     </div>
   )
